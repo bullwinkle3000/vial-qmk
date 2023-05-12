f6fe6f289a58221f78fc1cd033d4d537e613ecfa
Chris Crocker <chris.crocker7227@gmail.com>
2023-05-11 21:35:45 -0500

    Initial change

--- keyboards/handwired/wylderbuilds/trackball/3x5_3_track/keymaps/vial/keymap.c
+++ keyboards/handwired/wylderbuilds/trackball/3x5_3_track/keymaps/vial/keymap.c
@@ -4,37 +4,62 @@
 #define _QWERTY 0
 #define _LOWER 1
 #define _RAISE 2
+#define _MOUSE 2
 
 #define RAISE MO(_RAISE)
 #define LOWER MO(_LOWER)
+#define MOUSE MO(_MOUSE)
+
+const uint16_t PROGMEM enter_combo1[] = {KC_D, KC_F, COMBO_END};
+const uint16_t PROGMEM enter_combo2[] = {KC_J, KC_K, COMBO_END};
+const uint16_t PROGMEM tab_combo[] = {KC_S, KC_D, COMBO_END};
+
+combo_t key_combos[COMBO_COUNT] = {
+    COMBO(enter_combo1, KC_ENT),
+    COMBO(enter_combo2, KC_ENT),
+    COMBO(tab_combo, KC_TAB),
+};
+
+// void pointing_device_init_user(void) {
+//     set_auto_mouse_enable(true);         // always required before the auto mouse feature will work
+// }
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_split_3x5_3(
         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
+        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                    KC_H,    KC_J,    KC_K,    KC_L,    KC_QUOT,
         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-                                      RAISE, KC_SPC, KC_BSPC,      LOWER,
-                                                                   KC_ENT,
-                                                                KC_LALT
+                                   KC_LCTL, LOWER, KC_SPC,           KC_RSFT,
+                                                                   RAISE,
+                                                                KC_BSPC
         ),
 
     [_LOWER] = LAYOUT_split_3x5_3(
-        _______,_______,_______,_______,KC_LBRC,                     KC_RBRC, KC_P7 , KC_P8 , KC_P9 ,KC_PLUS,
-        KC_HOME,KC_PGUP,KC_PGDN,KC_END ,KC_LPRN,                     KC_RPRN, KC_P4 , KC_P5 , KC_P6 ,KC_MINS,
-        _______,_______,_______,_______,SNIPING,                     _______, KC_P1 , KC_P2 , KC_P3 ,KC_EQL,
-                                      KC_BTN3, KC_BTN1,DRGSCRL,    _______,
-                                                                   _______,
-                                                                QK_BOOT
+        KC_ESC,     _______,    DRGSCRL,    LALT(KC_F4), KC_KB_VOLUME_UP,        KC_BSLS, KC_7 , KC_8 , KC_9 ,KC_MINS,
+        LCTL(KC_A), KC_BTN3,    KC_DEL,     KC_BTN1,     KC_KB_VOLUME_DOWN,      KC_PAST, KC_4 , KC_5 , KC_6 ,KC_PLUS,
+        LCTL(KC_Z), LCTL(KC_X), LCTL(KC_C), LCTL(KC_V),  _______,                KC_PPLS, KC_1 , KC_2 , KC_3 ,KC_EQL,
+                                            QK_BOOT,     _______, _______,       KC_PDOT,
+                                                                               KC_0,
+                                                                            KC_BSPC
 
         ),
 
     [_RAISE] = LAYOUT_split_3x5_3(
-        KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                     KC_F6  , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,
-        _______,_______,_______,_______,KC_LBRC,                     KC_RBRC,_______,KC_NLCK,KC_INS ,KC_SLCK,
-        KC_LEFT,KC_UP  ,KC_DOWN,KC_RGHT,KC_LPRN,                     KC_RPRN,KC_MPRV,KC_MPLY,KC_MNXT,KC_VOLD,
-                                        _______,_______,QK_BOOT,   _______,
-                                                                   _______,
-                                                                 _______
+        KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                    KC_CIRC, KC_AMPR, KC_ASTR, KC_MINS, KC_EQL,
+        KC_CAPS, _______, KC_LBRC, KC_RBRC, _______,                    KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_SCLN,
+        KC_NUM,  KC_SCRL, KC_LPRN, KC_RPRN, _______,                    KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_SLSH,
+                                   _______, KC_LSFT, _______,           QK_BOOT,
+                                                                       _______,
+                                                                _______
+        ),
+
+    [_MOUSE] = LAYOUT_split_3x5_3(
+        _______, _______,  _______, _______, _______,                      _______, _______, _______, _______, _______ ,
+        _______, _______,  _______, _______, _______,                      _______, KC_BTN1, KC_BTN2, _______, _______,
+        _______, _______,  _______, _______, _______,                      _______, _______, _______, _______, _______,
+                                    _______, _______, _______,             _______,
+                                                                       _______,
+                                                                _______
         )
 };
 
@@ -48,6 +73,7 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation) {
 }
 
 static void render_wylderbuilds(void) {
+
     static const char PROGMEM wylderbuilds[] = {
         // 'dark wylderbuilds_oled_name', 128x32px
         0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
